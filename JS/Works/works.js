43ac2578b6aec8f2dae27ce5a3def9ad200ab852
LingBrian <1745087927@qq.com>
2023-01-01 16:33:20 +0800

    Automatically Update

---
+++ JS/Works/works.js
@@ -0,0 +1,12 @@
+function view(){
+    var showcase_bg = document.getElementById("showcase_bg");
+    showcase_bg.style.display = "block";
+    var showcase = document.getElementById("showcase");
+    showcase.style.display = "block";
+}
+function view_close(){
+    var showcase_bg = document.getElementById("showcase_bg");
+    showcase_bg.style.display = "none";
+    var showcase = document.getElementById("showcase");
+    showcase.style.display = "none";
+}
